dac782856abcdfe4a631d5ab760f6883661051e2
Svetlana Khomenko <homenko-svetlana87@mail.ru>
2023-06-15 14:55:46 +0300

    First correction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
 
     val discount = (sumBuy * 5)/100
     val calculation = if (sumBuy <= 1000) 0 else if (sumBuy > 1_001 && sumBuy < 10_000)  100 else discount
-    val result = if ( regularly == true) ((sumBuy-calculation)*1)/100 else calculation
+    val result = if (regularly) ((sumBuy-calculation)*1)/100 else calculation
     val itog = sumBuy - calculation - result
 
     println("Сумма вашей покупки $sumBuy руб. скидка составила $calculation руб")
